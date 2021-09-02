d891e21b9c7d17898048930bf92a36bdd55d682e
Jim Forristal <70030066+nwclassic@users.noreply.github.com>
2021-09-02 13:02:40 -0700

    adding sudo to docker run

--- entry-script.sh
+++ entry-script.sh
@@ -5,8 +5,8 @@ sudo systemctl start docker
 sudo usermod -aG docker ec2-user
 
 # Use this to run from you local machine with "user-data" inline
-# docker run -p 8080:80 nginx
+# sudo docker run -p 8080:80 nginx
 
 # Use this to run remotely with provisioner file first, then inline
-docker run -dp 8080:80 nginx
+sudo docker run -dp 8080:80 nginx
 
